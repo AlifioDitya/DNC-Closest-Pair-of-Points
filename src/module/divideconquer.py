243b1a42e581d81da54b655428de4041fd84561f
AlifioDitya <alifioditya@gmail.com>
2023-02-22 22:24:23 +0700

    Fix euclidian distance and test Point class

--- src/module/divideconquer.py
+++ src/module/divideconquer.py
@@ -1,11 +1,8 @@
 from Point import Point
 
-def main():
+def test():
     oldPoint = Point(1,2,3)
     newPoint = Point(4,5,6)
     distance = newPoint.distance_to(oldPoint)
     print(distance)
     return
-
-if __name__ == '__main__':
-    main()
