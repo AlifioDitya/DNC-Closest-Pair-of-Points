a823e3884256d75751c0d168c10f56106a8b0607
maikeljh <mjh191003@gmail.com>
2023-02-24 01:00:44 +0700

    change wrong output in testing

--- src/module/testing.py
+++ src/module/testing.py
@@ -3,6 +3,6 @@ from module.bruteforce import brute_force_closest_pair
 def testBF(points):
     # Compare with brute force
     p1, p2 = brute_force_closest_pair(points)
-    print("Minimum distance with DNC :", p1.distance_to(p2))
-    print("Closest pair of points with DNC:", (p1,p2))
+    print("Minimum distance with BF :", p1.distance_to(p2))
+    print("Closest pair of points with BF:", (p1,p2))
     print("")
